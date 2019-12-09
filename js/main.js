ab214b54a1ff67393a0ee4d30380cd35ac602c73
morfiy <edurnev@keaz.ru>
2019-12-09 16:39:06 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -36,14 +36,9 @@ $(document).ready(function () {
     });
 });
 
-var itemsCount=2;
-
-if (window.matchMedia("(max-width: 767px)").matches) {
-    itemsCount=1;
-}
 var owl = $('.owl-carousel');
 owl.owlCarousel({
-    items:itemsCount,
+    items:1,
     loop:true,
     margin:0,
     nav:true,
@@ -57,3 +52,24 @@ $('.play').on('click',function(){
 $('.stop').on('click',function(){
     owl.trigger('autoplay.stop.owl')
 })
+
+var wrapper = document.querySelector(".header-slogan");
+var text = document.querySelector(".slogan-text");
+
+var textCont = text.textContent;
+text.style.display = "none";
+
+for (var i = 0; i < textCont.length; i++) {
+    (function(i) {
+        setTimeout(function() {
+            // Created textNode to append
+            var texts = document.createTextNode(textCont[i])
+            var span = document.createElement('span');
+            span.appendChild(texts);
+
+            span.classList.add("wave");
+            wrapper.appendChild(span);
+
+        }, 75 * i);
+    }(i));
+}
