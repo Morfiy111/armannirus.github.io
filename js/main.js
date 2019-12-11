9cf9d45c8237fd02ae2dcf4c79028cec585b4eb1
morfiy <edurnev@keaz.ru>
2019-12-11 10:14:06 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -13,14 +13,6 @@ $(document).ready(function(){
         return false;
     });
 });
-$(document).ready(function(){
-    $(".navbar-nav").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
-});
 
 $(document).ready(function () {
     $(".navbar-toggle").on("click", function () {
